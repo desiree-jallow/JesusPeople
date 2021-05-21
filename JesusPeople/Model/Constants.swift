4f4321085e65472f36e7e2beb55928acc60dfa81
Desiree Jallow <60536540+desiree-jallow@users.noreply.github.com>
2021-05-20 21:21:38 -0500

    work on design of welcome page

---
+++ JesusPeople/Model/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  JesusPeople
+//
+//  Created by Desiree on 5/20/21.
+//
+
+import Foundation
+
+struct Constants {
+    let apiKey = "AIzaSyBALl3j6ofr3CR0QQD6xAiI45zU2riAatM"
+}
---
+++ JesusPeople/View/shadowView.swift
@@ -0,0 +1,22 @@
+//
+//  shadowView.swift
+//  JesusPeople
+//
+//  Created by Desiree on 5/20/21.
+//
+
+import UIKit
+
+class shadowView: UIView {
+    
+    avatarImageView?.layer.cornerRadius = 32
+            addSubview(avatarImageView)
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
