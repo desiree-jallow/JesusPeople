1f0c44d04c2518999dd9f0912e7f6f3d612c8955
Desiree Jallow <60536540+desiree-jallow@users.noreply.github.com>
2021-05-24 15:45:55 -0500

    create closure to send data to view controller

--- JesusPeople/Model/PlaylistDataModel.swift
+++ JesusPeople/Model/PlaylistDataModel.swift
@@ -7,77 +7,23 @@
 
 import Foundation
 
-    struct SermonModel {
-        let title:String
-        let thumbnail:String
+//if doesnt work check if any are optional
+
+
+struct PlaylistDataModel:Codable {
+    let items:[PlaylistItem]
+    
 }
 
-    struct PlaylistDataModel:Codable {
-        let kind:String
-        let etag:String
-        let nextPageToken:String
-        let items:[PlaylistItem]
-        let pageInfo:Info
+struct PlaylistItem:Codable {
+    let contentDetails:Details
+    
+}
+struct Details:Codable {
+    let videoId:String    
 }
 
-    struct Info:Codable {
-        let totalResults:Int
-        let resultsPerPage:Int
-}
 
-    struct PlaylistItem:Codable {
-        let kind:String
-        let etag:String
-        let id:String
-        let contentDetails:Details
-     
-}
-    struct Details:Codable {
-        let videoId:String
-        let note:String
-        let videoPublishedAt:String
-}
-
-struct VideoDataModel:Codable {
-    let kind:String
-    let etag:String
-    let nextPageToken:String
-    let prevPageToken:String
-    let items:[VideoItem]
-    let pageInfo:Info
-}
-
-struct VideoItem:Codable {
-    let kind:String
-    let etag:String
-    let id:String
-    let snippet:VideoSnippet
-}
-    struct VideoSnippet:Codable {
-        let publishedAt:String
-        let channelID:String
-        let title:String
-        let description:String
-        let thumbnails:[String:ThumbnailsValue]
-        let tags:[String]
-        let channelTitle:String
-        let categoryId:String
-        let liveBroadcastContent:String
-        let defaultLanguage:String
-        let localized:Localized
-        let defaultAudioLanguage:String
-    }
-
-struct Localized:Codable {
-    let title:String
-    let description:String
-}
-    struct ThumbnailsValue:Codable {
-        let url:String
-        let width:String
-        let height:String
-          
-    }
 
 
 
