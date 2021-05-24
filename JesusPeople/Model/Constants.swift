fa62a02decfe13eceaa65f17e9d58d442a0f3c4d
Desiree Jallow <60536540+desiree-jallow@users.noreply.github.com>
2021-05-24 15:44:32 -0500

    create closure to send data to view controller

---
+++ JesusPeople/Model/Constants.swift
@@ -0,0 +1,19 @@
+//
+//  Constants.swift
+//  JesusPeople
+//
+//  Created by Desiree on 5/23/21.
+//
+
+import Foundation
+
+struct Constants {
+    static let api = "AIzaSyBALl3j6ofr3CR0QQD6xAiI45zU2riAatM"
+    static let channelId = "UCDzdO9eXlpKUxP3FI5q-T8Q"
+    static let playlistId = "UUDzdO9eXlpKUxP3FI5q-T8Q"
+    static let exampleVideoId = "3EUs1Y4ZHPc"
+    
+    static let playListUrl =  "https://www.googleapis.com/youtube/v3/playlistItems?part=contentDetails&playlistId=UUDzdO9eXlpKUxP3FI5q-T8Q&key=AIzaSyBALl3j6ofr3CR0QQD6xAiI45zU2riAatM&maxResults=15"
+    
+    static let videoUrl =  "https://www.googleapis.com/youtube/v3/videos?part=snippet,liveStreamingDetails&id=3EUs1Y4ZHPc&key=AIzaSyBALl3j6ofr3CR0QQD6xAiI45zU2riAatM"
+}
