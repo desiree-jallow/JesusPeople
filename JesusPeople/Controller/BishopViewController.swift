ca4225e34a7b2c3dfc698f2e17cebc546171ca8d
Desiree Jallow <60536540+desiree-jallow@users.noreply.github.com>
2021-05-27 13:34:11 -0500

    work on hamburger menu items

--- JesusPeople/Controller/BishopViewController.swift
+++ JesusPeople/Controller/BishopViewController.swift
@@ -1,50 +1,33 @@
 //
-//  BibleViewController.swift
+//  BishopViewController.swift
 //  JesusPeople
 //
-//  Created by Desiree on 5/20/21.
+//  Created by Desiree on 5/27/21.
 //
 
 import UIKit
 import WebKit
 
-class BibleViewController: UIViewController, WKNavigationDelegate {
-    var webView: WKWebView!
-    var spinner = UIActivityIndicatorView(style: .large)
+class BishopViewController: UIViewController,WKNavigationDelegate {
+    @IBOutlet weak var webView: WKWebView!
+    @IBOutlet weak var spinner: UIActivityIndicatorView!
     
     override func viewWillAppear(_ animated: Bool) {
-        showSpinner()
-    }
-    
-    override func loadView() {
-        super.loadView()
-        webView = WKWebView()
-        webView.navigationDelegate = self
-        view = webView
-
-    }
-    
-    func showSpinner() {
-        spinner.translatesAutoresizingMaskIntoConstraints = false
         spinner.startAnimating()
-        view.addSubview(spinner)
-
-        spinner.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        spinner.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        webView.navigationDelegate = self
         
-        if let url = URL(string: Constants.bibleUrl) {
+        
+        if let url = URL(string: Constants.bishopUrl) {
             webView.load(URLRequest(url: url))
             webView.allowsBackForwardNavigationGestures = true
-        }
-       
         // Do any additional setup after loading the view.
-        
-        
+        }
     }
+  
     
     func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
         if webView.isLoading {
@@ -53,9 +36,8 @@ class BibleViewController: UIViewController, WKNavigationDelegate {
         spinner.stopAnimating()
     }
     
-   
     
-    
+
     /*
     // MARK: - Navigation
 
--- JesusPeople/Controller/MinistriesViewController.swift
+++ JesusPeople/Controller/MinistriesViewController.swift
@@ -1,51 +1,36 @@
 //
-//  BibleViewController.swift
+//  MinistriesViewController.swift
 //  JesusPeople
 //
-//  Created by Desiree on 5/20/21.
+//  Created by Desiree on 5/27/21.
 //
 
 import UIKit
 import WebKit
 
-class BibleViewController: UIViewController, WKNavigationDelegate {
-    var webView: WKWebView!
-    var spinner = UIActivityIndicatorView(style: .large)
+class MinistriesViewController: UIViewController, WKNavigationDelegate{
+    @IBOutlet weak var webView: WKWebView!
+    @IBOutlet weak var spinner: UIActivityIndicatorView!
+    
     
     override func viewWillAppear(_ animated: Bool) {
-        showSpinner()
-    }
-    
-    override func loadView() {
-        super.loadView()
-        webView = WKWebView()
-        webView.navigationDelegate = self
-        view = webView
-
-    }
-    
-    func showSpinner() {
-        spinner.translatesAutoresizingMaskIntoConstraints = false
         spinner.startAnimating()
-        view.addSubview(spinner)
-
-        spinner.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        spinner.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        if let url = URL(string: Constants.bibleUrl) {
+        webView.navigationDelegate = self
+     
+       
+        if let url = URL(string: Constants.ministiesUrl) {
             webView.load(URLRequest(url: url))
             webView.allowsBackForwardNavigationGestures = true
-        }
        
+        }
         // Do any additional setup after loading the view.
-        
-        
     }
     
+   
     func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
         if webView.isLoading {
             return
@@ -53,9 +38,7 @@ class BibleViewController: UIViewController, WKNavigationDelegate {
         spinner.stopAnimating()
     }
     
-   
-    
-    
+
     /*
     // MARK: - Navigation
 
