f87aef0f0806a0d276d1db4250e10cd2bb9a9dff
Desiree Jallow <60536540+desiree-jallow@users.noreply.github.com>
2021-06-01 14:38:46 -0500

    giving controller fix, navigation issue fix

--- JesusPeople/Controller/BishopViewController.swift
+++ JesusPeople/Controller/BishopViewController.swift
@@ -8,35 +8,19 @@
 import UIKit
 import WebKit
 
-class BishopViewController: UIViewController,WKNavigationDelegate {
-    @IBOutlet weak var webView: WKWebView!
-    @IBOutlet weak var spinner: UIActivityIndicatorView!
-    
-    override func viewWillAppear(_ animated: Bool) {
-        spinner.startAnimating()
-    }
+class BishopViewController: WebViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        webView.navigationDelegate = self
-        
         
             if let url = URL(string: Constants.bishopUrl) {
                 webView.load(URLRequest(url: url))
                 webView.allowsBackForwardNavigationGestures = true
-            // Do any additional setup after loading the view.
             
         }
      
     }
   
-    
-    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
-        if webView.isLoading {
-            return
-        }
-        spinner.stopAnimating()
-    }
     
     
 
