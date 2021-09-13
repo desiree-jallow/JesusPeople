5492cbbb3a820ce3e24569e1797ac22d147d3092
Desiree Jallow <60536540+desiree-jallow@users.noreply.github.com>
2021-09-13 18:50:41 -0500

    adjust social view spinner to stop when view is loaded

--- JesusPeople/Controller/SocialViewController.swift
+++ JesusPeople/Controller/SocialViewController.swift
@@ -33,21 +33,9 @@ class SocialViewController: UIViewController, WKNavigationDelegate {
         
     
     func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
-        if webView.isLoading {
-            return
+        if !webView.isLoading {
+            spinner.stopAnimating()
+            spinner.hidesWhenStopped = true
         }
-        spinner.stopAnimating()
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
-    
 }
